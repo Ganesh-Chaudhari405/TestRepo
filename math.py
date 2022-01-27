d07951e4be005b9b629a8affe3f91322c3f7f682
Ganesh Chaudhari <69956058+Ganesh-Chaudhari405@users.noreply.github.com>
2022-01-27 12:04:11 +0530

    Math file

---
+++ math.py
@@ -0,0 +1,16 @@
+
+//add Ganesh 
+
+//add Chaudhari 
+
+def add(x,y):
+    pass  // by master branch
+//sub
+def subtract(x,y):
+    pass // sub imp by bug456
+//mul
+def multiply(x,y):
+    pass
+//divide
+def divide(x,y):
+    pass
